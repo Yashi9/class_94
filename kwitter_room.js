7fbfc409d55e3caaa1a3568bafe1c1ebe68907cf
Yashi9 <91045663+Yashi9@users.noreply.github.com>
2022-06-20 11:01:14 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBHVO0wo-UHiXdWOJ02wM0grRjYUoZfq8E",
+    authDomain: "lets-chat-a2b1b.firebaseapp.com",
+    databaseURL: "https://lets-chat-a2b1b-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-a2b1b",
+    storageBucket: "lets-chat-a2b1b.appspot.com",
+    messagingSenderId: "675201621096",
+    appId: "1:675201621096:web:b2e5b8d343a4cb61ab2fdd"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
